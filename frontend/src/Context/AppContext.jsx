6b074a807b621b15607ffa30546f8383b603b685
Ayush <139117702+ayush-1560@users.noreply.github.com>
2025-01-19 10:27:52 +0530

    Update AppContext.jsx

--- frontend/src/Context/AppContext.jsx
+++ frontend/src/Context/AppContext.jsx
@@ -6,7 +6,7 @@ import {toast} from 'react-toastify'
 
 const AppContextProvider = (props) =>{
     const currencySymbol='$';
-    const backendUrl = import.meta.env.VITE_BACKEND_URL;
+    const backendUrl ="https://prescripto-backend-wffy.onrender.com";
     const [doctors,setDoctors] = useState([]);
     const [token,setToken] = useState(localStorage.getItem('token') ?localStorage.getItem('token') :false);
     const [userData,setUserData] = useState(false);
